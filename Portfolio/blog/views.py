f7dc9512ed166c07f4084340a6e6bb9d6e3c29ea
HAMIN LEE <66108236+haa741123@users.noreply.github.com>
2025-01-04 05:38:26 +0900

    url 변경, static 폴더 위치 변경

--- Portfolio/blog/views.py
+++ Portfolio/blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context= {}
     return render(request,'index.html', context)
+
+def main(request):
+    context= {}
+    return render(request,'main.html', context)
